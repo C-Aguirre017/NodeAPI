a6943e3ce0c8a13e1147f6d06b2de34be50e71e7
Carlos Aguirre <c.aguirre017@gmail.com>
2016-07-19 00:21:38 -0400

    Env Production

--- configuration/production.js
+++ configuration/production.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
-var uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/Evaluame';
+var uri = process.env.MONGOLAB_URI || process.env.MONGODB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/Evaluame';
 
 global.db = mongoose.connect(uri, function (err, res) {
      if (err) {
-       console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+       console.log ('[!] ERROR connecting to: ' + uristring + '. ' + err);
      } else {
-       console.log ('Succeeded connected to: ' + uristring);
+       console.log ('[*] Succeeded connected to: ' + uristring);
      }
    });
